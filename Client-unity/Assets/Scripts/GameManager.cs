795e7ff0f9ae5f47721d00fc4c405b4a5e4b4489
Cong Zeng <3512145578@qq.com>
2026-05-25 01:43:46 +0800

    添加AuthToken功能支持同用户重连时服务器依据Token识别用户
    在游戏启动时初始化身份令牌 (AuthToken)，并在 ** 游戏管理器 (GameManager)** 中完成网络连接逻辑绑定：
    调用AuthToken.Init()完成令牌初始化
    注册OnConnect连接回调处理器：保存 / 打印接收到的令牌和用户身份信息
    若本地已有缓存令牌，将其附加到网络构建器中
    在Update方法中调用Conn.FrameTick()驱动网络帧更新
    补充必要的System系统命名空间引用

--- Client-unity/Assets/Scripts/GameManager.cs
+++ Client-unity/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using SpacetimeDB;
 using SpacetimeDB.Types;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,15 +13,35 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // 必须先初始化 AuthToken，才能安全地访问 AuthToken.Token
+        AuthToken.Init();
+
         DbConnectionBuilder<DbConnection> builder = DbConnection.Builder();
         builder.WithUri(ServerUri);
         builder.WithModuleName(ModuleName);
+
+        builder.OnConnect(HandleConnect);
+        // 如果之前已经连接过服务器并且保存了认证令牌，那么在重新连接时可以直接使用这个令牌进行认证，无需再次输入用户名和密码
+        if (AuthToken.Token != "")
+        {
+            builder.WithToken(AuthToken.Token);
+        }
         Conn = builder.Build();
 
     }
+
+    private void HandleConnect(DbConnection conn, Identity identity, string token)
+    {
+        // 连接成功后会得到一个身份标识（Identity）和一个认证令牌（AuthToken）都是独一无二的身份认证的字符串，可以将它们保存起来以便后续使用
+        AuthToken.SaveToken(token);
+        print(token);
+        print(identity);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        // 必须在每帧驱动网络消息，否则任何回调（包含 OnConnect）都不会被触发
+        Conn?.FrameTick();
     }
 }
